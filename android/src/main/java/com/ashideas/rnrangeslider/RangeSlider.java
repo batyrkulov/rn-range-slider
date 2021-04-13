d86e829fd66d210abe8525209193341a4ed80e57
Aidos <aidosbatyrkulov@gmail.com>
2021-04-13 21:42:35 +0600

    Update RangeSlider.java

--- android/src/main/java/com/ashideas/rnrangeslider/RangeSlider.java
+++ android/src/main/java/com/ashideas/rnrangeslider/RangeSlider.java
@@ -465,7 +465,7 @@ public class RangeSlider extends View {
             }
         }
 
-        if (labelStyle == LabelStyle.NONE || activeThumb == THUMB_NONE) {
+        if (labelStyle == LabelStyle.NONE) {
             return;
         }
 
